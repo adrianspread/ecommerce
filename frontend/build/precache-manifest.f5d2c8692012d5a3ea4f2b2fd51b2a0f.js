65dbb1c22756cc8c15b6b9c7d53ec698e289e483
Adrian Wysocki <dobrystuff77@gmail.com>
2020-07-24 11:28:00 +0200

    mobile version

---
+++ frontend/build/precache-manifest.f5d2c8692012d5a3ea4f2b2fd51b2a0f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7376946552c9f54d1b224a722f1bf8b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e7990cbed18cebd009b",
+    "url": "/static/css/main.33b2a850.chunk.css"
+  },
+  {
+    "revision": "e18642f04b301f55bd96",
+    "url": "/static/js/2.8c9054ef.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.8c9054ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e7990cbed18cebd009b",
+    "url": "/static/js/main.b69f9643.chunk.js"
+  },
+  {
+    "revision": "4fda77b36933e7b909e2",
+    "url": "/static/js/runtime-main.e6ecbd0d.js"
+  },
+  {
+    "revision": "ae427edb9414023b7458031f6c5b5bb5",
+    "url": "/static/media/arrow.ae427edb.svg"
+  },
+  {
+    "revision": "99a849c12489c63ec9cfa03007ad20d8",
+    "url": "/static/media/menu.99a849c1.svg"
+  }
+]);
