87b087591689312a7ad60cd6e5a0c5483347288e
Adrian Wysocki <dobrystuff77@gmail.com>
2020-07-29 18:15:17 +0200

    CSS changes

--- frontend/src/core/Menu.js
+++ frontend/src/core/Menu.js
@@ -1,129 +1,140 @@
-import React, { Fragment, useState }  from "react";
+import React, { Fragment, useState } from "react";
 import { Link, withRouter } from "react-router-dom";
 import { signout, isAuthenticated } from "../auth";
 import { itemTotal } from "./cartHelpers";
 import Hamburger from "./menu.svg";
-import MobileMenu from "./MobileMenu"
+import MobileMenu from "./MobileMenu";
 
+function Menu(props) {
+  const [navOpen, setNavOpen] = useState(false);
+  const [move, setMove] = useState("mobile-menu");
+  const history = props.history;
 
-function Menu (props) {
+  const isActiv = (history, path) => {
+    if (history.location.pathname === path) {
+      return {
+        color: "black",
+        background: "#ffffff",
+        border: "1px solid white"
+      };
+    } else {
+      return { color: "#ffffff" };
+    }
+  };
 
-    const [navOpen, setNavOpen] = useState(false)
-    const [move, setMove] = useState("mobile-menu");
-    const history = props.history;
+  return (
+    <>
+      <MobileMenu move={move} onClick={() => setMove("mobile-menu")} />
 
-    const isActiv = (history, path) => {
-      if (history.location.pathname === path) {
-        return { color: "black", background: "#ffffff", border: "1px solid white" };
-      } else {
-        return { color: "#ffffff" };
-      }
-    };
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          background: "#28A745",
+          height: "43px"
+        }}
+      >
+        <div className="logo">wetreenow!</div>
+        <ul className="nav nav-tabs bg-success desktop-only">
+          <li className="nav-item">
+            <Link className="nav-link" to="/" style={isActiv(history, "/")}>
+              Home
+            </Link>
+          </li>
 
-
-    return (
-        <>
-            <MobileMenu move={move} onClick = {()=>setMove("mobile-menu")}/>
-
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "space-between",
-                background: "#28A745",
-                height: "43px"
-              }}
+          <li className="nav-item">
+            <Link
+              className="nav-link"
+              to="/shop"
+              style={isActiv(history, "/shop")}
             >
-                <div className="logo">wetreenow!</div>
-                <ul className="nav nav-tabs bg-success desktop-only">
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/" style={isActiv(history, "/")}>
-                      Home
-                    </Link>
-                  </li>
+              Plantation
+            </Link>
+          </li>
 
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/shop" style={isActiv(history, "/shop")}>
-                      Plantation
-                    </Link>
-                  </li>
+          <li className="nav-item">
+            <Link
+              className="nav-link"
+              to="/card"
+              style={isActiv(history, "/card")}
+            >
+              Basket{" "}
+              <sup>
+                <small className="cart-badge">{itemTotal()}</small>
+              </sup>
+            </Link>
+          </li>
 
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/card" style={isActiv(history, "/card")}>
-                      Basket{" "}
-                      <sup>
-                        <small className="cart-badge">{itemTotal()}</small>
-                      </sup>
-                    </Link>
-                  </li>
+          {isAuthenticated() && isAuthenticated().user.role === 0 && (
+            <li className="nav-item">
+              <Link
+                className="nav-link"
+                to="/user/dashboard"
+                style={isActiv(history, "/user/dashboard")}
+              >
+                Dashboard
+              </Link>
+            </li>
+          )}
 
-                  {isAuthenticated() && isAuthenticated().user.role === 0 && (
-                    <li className="nav-item">
-                      <Link
-                        className="nav-link"
-                        to="/user/dashboard"
-                        style={isActiv(history, "/user/dashboard")}
-                      >
-                        Dashboard
-                      </Link>
-                    </li>
-                  )}
+          {isAuthenticated() && isAuthenticated().user.role === 1 && (
+            <li className="nav-item">
+              <Link
+                className="nav-link"
+                to="/admin/dashboard"
+                style={isActiv(history, "/admin/dashboard")}
+              >
+                Dashboard
+              </Link>
+            </li>
+          )}
 
-                  {isAuthenticated() && isAuthenticated().user.role === 1 && (
-                    <li className="nav-item">
-                      <Link
-                        className="nav-link"
-                        to="/admin/dashboard"
-                        style={isActiv(history, "/admin/dashboard")}
-                      >
-                        Dashboard
-                      </Link>
-                    </li>
-                  )}
-
-                  {!isAuthenticated() && (
-                    <Fragment>
-                      <li className="nav-item">
-                        <Link
-                          className="nav-link"
-                          to="/signin"
-                          style={isActiv(history, "/signin")}
-                        >
-                          Login
-                        </Link>
-                      </li>
-                      <li className="nav-item">
-                        <Link
-                          className="nav-link"
-                          to="/signup"
-                          style={isActiv(history, "/signup")}
-                        >
-                          Signup
-                        </Link>
-                      </li>
-                    </Fragment>
-                  )}
-                  {isAuthenticated() && (
-                    <div>
-                      <li className="nav-item">
-                        <span
-                          className="nav-link"
-                          style={{ cursor: "pointer", color: "#ffffff" }}
-                          onClick={() =>
-                            signout(() => {
-                              history.push("/");
-                            })
-                          }
-                        >
-                          Signout
-                        </span>
-                      </li>
-                    </div>
-                  )}
-                </ul>
-                <div className="hamburger" onClick={()=>setMove("mobile-menu move")}><img src={Hamburger} className="icon"/></div>
-            </div>
-        </>
-    )
+          {!isAuthenticated() && (
+            <Fragment>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  to="/signin"
+                  style={isActiv(history, "/signin")}
+                >
+                  Login
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  to="/signup"
+                  style={isActiv(history, "/signup")}
+                >
+                  Signup
+                </Link>
+              </li>
+            </Fragment>
+          )}
+          {isAuthenticated() && (
+            <>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  style={{ cursor: "pointer", color: "#ffffff" }}
+                  onClick={() =>
+                    signout(() => {
+                      history.push("/");
+                    })
+                  }
+                >
+                  Signout
+                </Link>
+              </li>
+            </>
+          )}
+        </ul>
+        <div className="hamburger" onClick={() => setMove("mobile-menu move")}>
+          <img src={Hamburger} className="icon" />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default withRouter(Menu);
--- frontend/src/user/Signin.js
+++ frontend/src/user/Signin.js
@@ -5,8 +5,8 @@ import { signin, authenticate, isAuthenticated } from "../auth";
 
 const Signin = () => {
   const [values, setValues] = useState({
-    email: "adiadiadi@adi.com",
-    password: "adiadi1",
+    email: "",
+    password: "",
     error: "",
     loading: false,
     redirectToReferrer: false
